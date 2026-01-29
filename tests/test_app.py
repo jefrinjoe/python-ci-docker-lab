f84e761586cc928a4555e8a6594e9cb4f0486b84
Jefrin Joe <jefrinjoe@karunya.edu.in>
2026-01-29 12:50:11 +0530

    fix import path for pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,11 @@
+import sys
+import os
+
+# Add project root to Python path
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from app import add
+
 
 def test_add():
     assert add(2, 3) == 5
